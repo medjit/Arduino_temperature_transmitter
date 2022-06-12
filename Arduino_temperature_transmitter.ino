5ec6defce3ef742f143e246acbafe1ffb9672912
medjit <medjit.saitov@abv.bg>
2022-06-12 21:33:28 +0300

    Ds18b20 hande DEVICE_DISCONNECTED_C.

--- Arduino_temperature_transmitter.ino
+++ Arduino_temperature_transmitter.ino
@@ -2,12 +2,16 @@
 #include <DallasTemperature.h>
 
 #define DS18B20_GPIO  5
+#define BUZZER_GPIO   A5
+#define LED_GPIO      A1
 
 OneWire oneWire(DS18B20_GPIO);
 DallasTemperature sensors(&oneWire);
 
 void setup()
 {
+  pinMode(LED_GPIO, OUTPUT);
+  pinMode(BUZZER_GPIO, OUTPUT);
   Serial.begin(9600);
   sensors.begin();
   sensors.setResolution(9);
@@ -15,7 +19,14 @@ void setup()
 
 void loop()
 {
-  Serial.println("Temperature for the device 1 (index 0) is: " + (String)getTemperature());
+  digitalWrite(LED_GPIO, !digitalRead(LED_GPIO));
+  float temp = getTemperature();
+  if (temp == DEVICE_DISCONNECTED_C) {
+    digitalWrite(BUZZER_GPIO, HIGH);
+    delay(1000);
+    digitalWrite(BUZZER_GPIO, LOW);
+  }
+  Serial.println("Temperature for the device 1 (index 0) is: " + (String)temp);
   delay(500);
 }
 
